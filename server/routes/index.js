e8d8c79e042cf419b84fb50d19c38ec8ace58cdf
Segun Abisagbo <segebee@gmail.com>
2017-09-02 12:57:07 +0100

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var jwt = require('express-jwt');
 var auth = jwt({
-  secret: 'HansonPrintsFoto',
+  secret: 'haha',
   userProperty: 'payload'
 });
 
